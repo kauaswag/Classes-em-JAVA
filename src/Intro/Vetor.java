6f86e041395dfea4710f359bad718502376857a5
Kauaswag <kaualongui2017@gmail.com>
2025-10-07 19:35:05 -0300

    Classes em Java

---
+++ src/Intro/Vetor.java
@@ -0,0 +1,16 @@
+package Intro;
+
+public class Vetor {
+    public static void main(String[] args) {
+        int[] vetor;
+        vetor = new int[5];
+        vetor[0] = 10;
+        vetor[1] = 20;
+        vetor[2] = 30;
+        vetor[3] = 40;
+        vetor[4] = 50;
+        int[] vetor2 = { 1, 2, 3, 4, 5 };
+        System.out.println("Conteúdo do vetor [0]: " + vetor[0]);
+        System.out.println("Conteúdo de vetor2[4]: " + vetor2[4]);
+    }
+}
